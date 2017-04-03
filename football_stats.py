7504bda2725e4768e6c23943dc0f6f91b7f34e2b
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-04-03 15:21:43 -0400

    Attempted to write main() script to load and parse.

--- football_stats.py
+++ football_stats.py
@@ -4,9 +4,14 @@
 import urllib2
 from bs4 import BeautifulSoup
 
+def process_html(read_data):
+    player_stats = []
 
 
 def main():
     url = 'http://www.cbssports.com/nfl/stats/playersort/nfl/year-2016-season-post-category-touchdowns'
     get_data = urllib2.urlopen(url)
     read_data = BeautifulSoup(get_data.read())
+
+if __name__ == '__main__':
+    main()
--- weather.py
+++ weather.py
@@ -7,6 +7,6 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = 'http://www.cbssports.com/nfl/stats/playersort/nfl/year-2016-season-post-category-touchdowns'
+    url = 'http://www.cbssports.com/nfl/stats/playersort/nfl/year-2016-season-regular-category-touchdowns'
     get_data = urllib2.urlopen(url)
     read_data = BeautifulSoup(get_data.read())
