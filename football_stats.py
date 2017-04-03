3fd12a1dabf16e477119b2a6107df912dc68bca5
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-04-03 13:40:00 -0400

    Added basic download logic to main()

---
+++ football_stats.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib2
+from bs4 import BeautifulSoup
+
+
+
+def main():
+    url = 'http://www.cbssports.com/nfl/stats/playersort/nfl/year-2016-season-post-category-touchdowns'
+    get_data = urllib2.urlopen(url)
+    read_data = BeautifulSoup(get_data.read())
