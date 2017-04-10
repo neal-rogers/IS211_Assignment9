bbd59509edf0b183aebe7ba6c61f632d5c253bc6
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-04-10 14:53:52 -0400

    Not sure how to use urllib2 for this- added import and download changes.

--- football_stats.py
+++ football_stats.py
@@ -1,17 +1,19 @@
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib2
 from bs4 import BeautifulSoup
+import requests
 
-def process_html(read_data):
+def url_download(url):
+    get_data = requests.get(url)
+    return get_data.content
+
+def process_html(html_data):
     player_stats = []
+    new_table = html_data.find_all('tr',{'align':'right'})
 
-
-def main():
-    url = 'http://www.cbssports.com/nfl/stats/playersort/nfl/year-2016-season-post-category-touchdowns'
-    get_data = urllib2.urlopen(url)
-    read_data = BeautifulSoup(get_data.read())
+    for p in player_stats[:20]:
+        name = p.contents[0].get_text()
+    print player_stats
 
 if __name__ == '__main__':
-    main()
